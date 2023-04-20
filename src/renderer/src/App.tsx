7fa3dcf545048caf834c89adf973e2902605674e
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2023-04-20 14:49:23 -0300

    Configuring layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,7 @@
 import { Routes } from './Routes'
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
+
 import './styles/global.css'
 
 export function App() {
-	return (
-		<div className='w-screen h-screen bg-rotion-900 text-rotion-100 flex'>
-			<Sidebar/>
-			<div className='flex-1 flex flex-col max-h-screen'>
-				<Header/>
-
-				<Routes/>
-			</div>
-		</div>
-	)
+	return <Routes/>
 }
