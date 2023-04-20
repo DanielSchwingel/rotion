290a5b7599ef0c221cc5258c87cfb70b53b4dc91
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2023-04-20 15:50:26 -0300

    Text editor

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,3 +1,4 @@
+import { Editor } from '../components/Editor'
 import { ToC } from '../components/ToC'
 
 export function Document() {
@@ -6,7 +7,7 @@ export function Document() {
       <main className='flex-1 flex py-12 px-10 gap-8'>
          <aside className='hidden lg:block sticky top-0'>
             <span className='text-rotion-300 font-semibold text-xs'>
-               TABLE OF CONTENT
+               TABLE OF CONTENT 
             </span>
             <ToC.Root>
                <ToC.Link>Backend</ToC.Link>
@@ -17,7 +18,7 @@ export function Document() {
             </ToC.Root>
          </aside>
          <section className='flex-1 flex flex-col items-center'>
-asdasdsadasdsdsads
+            <Editor/>
          </section>
       </main>
    )
--- src/renderer/tailwind.config.js
+++ src/renderer/tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
 		}
 	},
 	plugins: [
+		require('@tailwindcss/typography'),
 		plugin(({ addUtilities }) => {
 			addUtilities({
 				'.region-drag': {
