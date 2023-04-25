d6837180ebca2c04fabae5eaacbb8f47aa2be7ae
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2023-04-25 09:40:49 -0300

    Application search

--- src/renderer/src/components/Sidebar/Search.tsx
+++ src/renderer/src/components/Sidebar/Search.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import { MagnifyingGlass } from 'phosphor-react'
 
+import { SearchBar } from '../SearchBar'
+
 export function Search() {
-  return (
-    <button className="flex mx-5 items-center gap-2 text-rotion-100 text-sm hover:text-rotion-50">
-      <MagnifyingGlass className="w-5 h-5" />
-      Busca rápida
-    </button>
-  )
+	const [ isSearchBarOpen, setIsSearchBarOpen] = useState(false)
+
+	function handleOpenChange(isOpen: boolean) {
+		setIsSearchBarOpen(isOpen)
+	 }
+
+	return (
+		<>
+			<button 
+				onClick={() => handleOpenChange(true)}
+				className='flex mx-5 items-center gap-2 text-rotion-100 text-sm hover:text-rotion-50'
+			>
+				<MagnifyingGlass className='w-5 h-51' />
+				Busca rápida
+			</button>
+			<SearchBar open={isSearchBarOpen} onOpenChange={handleOpenChange}/>
+
+		</>
+	)
 }
